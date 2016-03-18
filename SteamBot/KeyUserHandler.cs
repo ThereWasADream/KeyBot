a2bfc0222935fc62c38f9f6645a7fe99c7606f0d
ThereWasADream <jaypschultz@gmail.com>
2016-03-18 04:00:01 -0400

    regular backup

--- SteamBot/KeyUserHandler.cs
+++ SteamBot/KeyUserHandler.cs
@@ -18,8 +18,8 @@ namespace SteamBot
 	{
 		private const string BotVersion = "3.1.6";
 		public TF2Value UserMetalAdded, NonTradeInventoryMetal, InventoryMetal, BotMetalAdded, ExcessRefined, KeysToScrap, AdditionalRefined, ChangeAdded, LeftoverMetal;
-		public static TF2Value SellPricePerKey = TF2Value.FromRef(19.55); //high
-		public static TF2Value BuyPricePerKey = TF2Value.FromRef(19.22); //low
+		public static TF2Value SellPricePerKey = TF2Value.FromRef(19.33); //high
+		public static TF2Value BuyPricePerKey = TF2Value.FromRef(19.00); //low
 
 		int KeysCanBuy, NonTradeKeysCanBuy, ValidateMetaltoKey, PreviousKeys, UserKeysAdded, BotKeysAdded, InventoryKeys, NonTradeInventoryKeys, IgnoringBot, ScamAttempt, NonTradeScrap, Scrap, ScrapAdded, NonTradeReclaimed, Reclaimed, ReclaimedAdded, NonTradeRefined, Refined, RefinedAdded, InvalidItem, NumKeys, TradeFrequency;
         double Item;
@@ -84,6 +84,7 @@ namespace SteamBot
 		{
 			Cron.Interval = 10800000;
             CountInventory(false);
+            Bot.AcceptAllMobileTradeConfirmations();
             double FullCheck = Item / Slots;
             var HeadAdmin = new SteamID(Bot.Admins.First());
             if (FullCheck > 0.9)
